d7b30413accd51b07b999df71f0771af27814172
LoveIrene <chayunju1508@hanmail.net>
2025-08-24 21:58:04 +0900

    upd

---
+++ docs/init.js
@@ -0,0 +1,31 @@
+function tick(now){
+//   const tRunning = ((now - t0)/1000) / +pairs.tscale.r.value;
+//   const t = freeze.checked ? tHold : tRunning;
+    const tRunning = simTime(now);
+    const t = freeze.checked ? tHold : tRunning;
+    drawWave(t);
+    drawUVWStatic(t);
+    drawUVW3D(t);
+
+    drawAlphaBetaTime(t);
+    drawAlphaBeta(t);
+    drawAlphaBeta3D(t);
+
+    drawParkTime(t);
+    drawParkStatic(t);
+    drawPark3D(t);
+
+    drawInvParkTime(t);
+    drawInvParkStatic(t);
+    // drawInvPark3D(t);
+
+    drawInvClarkTime(t);
+    drawInvClarkStatic(t);
+    // drawInvClark3D(t);
+
+    updateMath(t);
+    updateMathUVW(t);
+    updateMathPark(t);
+
+    requestAnimationFrame(tick);
+}
--- docs/main.js
+++ docs/main.js
@@ -366,37 +366,7 @@
       }
     }
 
-    function tick(now){
-    //   const tRunning = ((now - t0)/1000) / +pairs.tscale.r.value;
-    //   const t = freeze.checked ? tHold : tRunning;
-      const tRunning = simTime(now);
-      const t = freeze.checked ? tHold : tRunning;
-      drawWave(t);
-      drawUVWStatic(t);
-      drawUVW3D(t);
-
-      drawAlphaBetaTime(t);
-      drawAlphaBeta(t);
-      drawAlphaBeta3D(t);
-
-      drawParkTime(t);
-      drawParkStatic(t);
-      drawPark3D(t);
-
-      drawInvParkTime(t);
-      drawInvParkStatic(t);
-      // drawInvPark3D(t);
-
-      drawInvClarkTime(t);
-      drawInvClarkStatic(t);
-      // drawInvClark3D(t);
-
-      updateMath(t);
-      updateMathUVW(t);
-      updateMathPark(t);
-
-      requestAnimationFrame(tick);
-    }
+    
 
     // kick off after DOM is ready (script is at the end, but ensure layout is done)
     window.addEventListener('load', init());
